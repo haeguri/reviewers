21c6e3788b415797849f63bd8d19f826b46ed6c6
haeguri <haegyun821@gmail.com>
2018-07-17 20:52:18 +0900

    monaco 지우고 react-monaco-editor 설치

--- reviewers-frontend/src/App.js
+++ reviewers-frontend/src/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
+import MonacoEditor from 'react-monaco-editor';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <header>
-          <h3>react project.</h3>
-        </header>
-        <p>
-          welcome.
-        </p>
-      </div>
-    );
-  }
+    constructor(props){
+        super(props);
+        this.state = {
+            code: '// type your code...',
+        }
+    }
+
+    editorDidMount(editor, monaco) {
+        console.log('editorDidMount', editor);
+        editor.focus();
+    }
+
+    onChange(newValue, e) {
+        console.log('onChange', newValue, e);
+    }
+
+    render() {
+        const code = this.state.code;
+        const options = {
+          selectOnLineNumbers: true
+        };
+        return (
+          <MonacoEditor
+            width="800"
+            height="600"
+            language="javascript"
+            theme="vs-dark"
+            value={code}
+            options={options}
+            onChange={() => this.onChange}
+            editorDidMount={() => this.editorDidMount}
+          />
+        );
+    }
 }
 
 export default App;
