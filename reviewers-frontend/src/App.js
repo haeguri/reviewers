648b2b374135d5933f059f6ffb0436d93c412440
haeguri <haegyun821@gmail.com>
2018-07-21 15:22:52 +0900

    glyph margin 영역 다루는 테스트

--- reviewers-frontend/src/App.js
+++ reviewers-frontend/src/App.js
@@ -1,17 +1,45 @@
 import React, { Component } from 'react';
 import Editor from './Editor/Editor';
+import { debug } from 'util';
 
 class App extends Component {
     constructor(props) {
         super(props);
         
         this.state = {
-            writeEditorCode: '// This is sample comment..',
-            readEditorCode: '// This is sample comment..',
+            writeEditorCode: (_=>{
+                const code = [];
+
+                for(let n of Array(10).fill(0)) {
+                    let line = '';
+                    for(let nn of Array(10).fill(n)) {
+                        line += nn;
+                    }
+
+                    code.push(line);
+                }
+
+                return code.join('\n');
+            })(),
+            readEditorCode: (_=>{
+                const code = [];
+
+                Array(10).fill(0).forEach((v, i) => {
+                    let line = '';
+
+                    Array(10).fill(0).forEach(() => {
+                        line += (i+1);
+                    })
+
+                    code.push(line);
+                });
+
+                return code.join('\n');
+            })()
         };
     }
 
-    editorDidMount(editor, monaco) {
+    readEditorDidMount(editor, monaco) {
         console.log('editorDidMount', editor);
 
         let viewZoneId;
@@ -24,6 +52,29 @@ class App extends Component {
                 domNode: domNode
             });
         });
+
+        editor.deltaDecorations([], [
+            {
+                range: new monaco.Range(3, 1, 3, 1),
+                options: {
+                    glyphMarginClassName: 'comment-btn'
+                }
+            }
+        ])
+
+        editor.onMouseDown(e => {
+            console.log('mouse down ! ', e);
+        })
+
+        editor.onMouseMove(e => {
+            // debugger;
+            console.log('EventTarget toString', e.target.toString());
+            console.log('Position : ', e.target.position);
+            // console.log('Range : ', e.target.range);
+            // console.log('Mouse Column : ', e.target.mouseColumn);
+            // console.log('Column : ', e.target.position.column);
+            // console.log('Line Number : ', e.target.position.lineNumber);
+        })
     }
 
     onWriteEditorChange(newValue, e) {
@@ -34,37 +85,27 @@ class App extends Component {
         console.log('write editor on change', newValue, e);
     }
 
-    onReadEditorChange(newValue, e) {
-        this.setState({
-            readEditorCode: newValue
-        });
-
-        console.log('read editor on change', newValue, e);
-    }
-
     render() {
         return (
             <div>
                 {/* Read-Only .*/}
                 <Editor 
                     options={{
-                        minimap: {
-                            enabled: false
-                        },
-                        readOnly: true
+                        minimap: { enabled: false },
+                        readOnly: true,
+                        glyphMargin: true,
                     }}
                     value={this.state.readEditorCode}
-                    editorDidMount={this.editorDidMount.bind(this)}
+                    editorDidMount={this.readEditorDidMount.bind(this)}
                     />
                 {/* Editable */}
                 <Editor 
                     options={{
-                        minimap: {
-                            enabled: false
-                        }
+                        minimap: { enabled: false }
                     }}
                     value={this.state.writeEditorCode}
                     onChange={this.onWriteEditorChange.bind(this)}
+                    
                     />
             </div>
         );
--- reviewers-frontend/src/Editor/Editor.js
+++ reviewers-frontend/src/Editor/Editor.js
@@ -3,8 +3,20 @@ import PropTypes from 'prop-types';
 import MonacoEditor from 'react-monaco-editor';
 
 class Editor extends Component {
+    eidtor = null;
+    monaco = null;
+
+    constructor(props) {
+        super(props);
+
+        Object.assign(props.options, { contextmenu: false });
+    }
+    
     editorDidMount(editor, monaco) {
         editor.focus();
+
+        this.editor = editor;
+        this.monaco = monaco;
 
         this.props.editorDidMount(editor, monaco);
     }
@@ -12,11 +24,11 @@ class Editor extends Component {
     render() {
         return (
             <MonacoEditor
-                width="600"
-                height="600"
-                language="javascript"
-                value={this.props.value}
                 theme="vs-dark"
+                width={this.props.width}
+                height={this.props.height}
+                language={this.props.language}
+                value={this.props.value}
                 options={this.props.options}
                 onChange={(newValue, e) => this.props.onChange(newValue, e)}
                 editorDidMount={(editor, monaco) => this.editorDidMount(editor, monaco)}
@@ -27,14 +39,20 @@ class Editor extends Component {
 
 Editor.defaultProps = {
     editorDidMount: _=>{},
-    onChange: _=>{}
+    onChange: _=>{},
+    language: 'javascript',
+    width: "600",
+    height: "600",
 };
 
 Editor.propTypes = {
+    language: PropTypes.string,
+    width: PropTypes.string,
+    height: PropTypes.string,
     value: PropTypes.string,
     options: PropTypes.object,
     editorDidMount: PropTypes.func,
-    onChange: PropTypes.func
+    onChange: PropTypes.func,
 };
 
 export default Editor;
