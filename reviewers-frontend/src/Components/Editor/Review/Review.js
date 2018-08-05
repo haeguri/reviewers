73994ec7bb061825358563cb7dfebf73b5ac09e2
haeguri <haegyun821@gmail.com>
2018-08-05 18:01:55 +0900

    styled-components 적용해보

--- reviewers-frontend/src/Components/Editor/Review/Review.js
+++ reviewers-frontend/src/Components/Editor/Review/Review.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const StyledWrapper = styled.div`
+    border: solid 1px black;
+    height: 100%;
+`
 
 const Review = (props) => {
     return (
-        <div 
-            className="review-form"
-        >
+        <StyledWrapper>
             <h1>This is Review Component!</h1>
             <button onClick={() => props.onCancelClick()}>Close Button</button>
-        </div>            
+        </StyledWrapper>
     )
 }
 
